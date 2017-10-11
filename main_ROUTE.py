6c36c2b67ff8ce38ea867f29681e1b92194765fa
Johannes Richter <hannesrichter@users.noreply.github.com>
2017-10-11 17:22:39 +0200

    Some comments and clean up of the code

--- main_ROUTE.py
+++ main_ROUTE.py
@@ -1,65 +1,81 @@
 #! /usr/bin/python3
-""""""
 
 import pygame as pg
 import numpy as np
-import math
-from random import random
-
 from sofm import KOHONENRING
 
-def random_circle(radius, off_x, off_y):
-    t = 2 * math.pi * random()
-    u = random() + random()
-    if u > 1:
-        r = 2 - u
-    else:
-        r = u
-    return radius * r * math.cos(t) + off_x, radius * r * math.sin(t) + off_y
+NUM_OF_POINTS = 21
+DRAW_STEP = 100
+MAX_STEP = 10000
+WINDOW_SIZE = 640
 
 def main():
-    size = 640
-    num_points = 21
-    
+    """Simulation of the Traveling Salesman Problem using Kohonen rings to approximate the
+    shortest roundtrip. The solution is not perfect but entertaining to watch"""
+    size = WINDOW_SIZE
+    num_points = NUM_OF_POINTS
+
+    # initialize the pygame system
     pg.init()
-    
+
+    # set the screen size and get teh window handle
     screen = pg.display.set_mode((size, size))
-    
-    
+    pg.display.set_caption("TSP with Kohonen rings")
 
-    while 1==1:
+    # for ever do...
+    while 1 == 1:
+        # initialize a new KOHONENRING instance
         my_sofm = KOHONENRING(2, num_points * 3, 0.99, 0.99999, 0.01)
-    
+
+        # generate random points in [0.0, 1.0] as our "towns"
         points = np.random.rand(num_points, 2)
-        index = 0
-        
+
+        # reset the tick counter
         tick = 0
-        while tick < 5000:
+
+        # while not reached MAX_STEP
+        while tick < MAX_STEP:
+            # get the all teh weights from the ring
             weights = my_sofm.get_weights()
-            screen.fill((255, 255, 255))
-            for it_x in range(weights.shape[0]):
-                pos_start = weights[it_x] * size
-                    
-                if it_x < weights.shape[0] - 1:
-                    pos_stop1 = weights[it_x + 1] * size
-                    pg.draw.line(screen, (0,0,0), pos_start, pos_stop1)
-                else:
-                    pos_stop1 = weights[0] * size
-                    pg.draw.line(screen, (0,0,0), pos_start, pos_stop1)
-            
-            for point in points:
-                pos_circle = point * size
-                pg.draw.circle(screen, (255,0,0), (int(pos_circle[0]), int(pos_circle[1])), 4)
 
-            pg.display.update()
-            _, deltas = my_sofm.train(points[np.random.randint(0, num_points)])
+            # do we need to draw now?
+            if tick % DRAW_STEP == 0:
+                # fill all white
+                screen.fill((255, 255, 255))
 
+                # for each weight in all the weights
+                for it_x in range(weights.shape[0]):
+                    # mark the start of the line
+                    pos_start = weights[it_x] * size
+
+                    # are we the last one?
+                    if it_x < weights.shape[0] - 1:
+                        # mark the end of the line and draw
+                        pos_stop1 = weights[it_x + 1] * size
+                        pg.draw.line(screen, (0, 0, 0), pos_start, pos_stop1)
+                    else:
+                        # mark the first neuron as the end and draw -> this creates a ring
+                        pos_stop1 = weights[0] * size
+                        pg.draw.line(screen, (0, 0, 0), pos_start, pos_stop1)
+
+                    # draw the neuron as circle
+                    pg.draw.circle(screen, (0, 0, 255), (int(pos_start[0]), int(pos_start[1])), 2)
+
+                # for each point in the list of "towns"
+                for point in points:
+                    # scale the position according to the window and draw
+                    pos_circle = point * size
+                    pg.draw.circle(screen, (255, 0, 0), (int(pos_circle[0]), int(pos_circle[1])), 4)
+                # make the changes visible on the screen
+                pg.display.update()
+
+            # perform a training step with one random "town" from the point list
+            my_sofm.train(points[np.random.randint(0, num_points)])
+
+            # increment the tick counter
             tick += 1
 
-            index += 1
-            if index >= num_points:
-                index = 0
-
+            # handle the events to see if someone wants to close the window
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     pg.quit()
